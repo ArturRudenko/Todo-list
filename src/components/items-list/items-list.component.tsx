87ad25e815fbe476fb8a35e75b543a0e4d5fcfe3
ArturRudenko <sparksbridge@gmail.com>
2021-06-23 16:25:21 -0700

    fix(items-list): removed unused import

--- src/components/items-list/items-list.component.tsx
+++ src/components/items-list/items-list.component.tsx
@@ -1,8 +1,6 @@
-import type {FC, ReactNode} from 'react';
+import type {FC} from 'react';
 import styles from './items-list.module.scss';
 
-const ItemsList: FC = ({children}) => (
-  <article className={styles.root}>{children}</article>
-);
+const ItemsList: FC = ({children}) => <article className={styles.root}>{children}</article>;
 
 export {ItemsList};
