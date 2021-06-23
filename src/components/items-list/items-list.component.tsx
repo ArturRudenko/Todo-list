e0962557829f67a04387c39a3c3be5b789a47e9a
Artur Rudenko <64748494+ArturRudenko@users.noreply.github.com>
2021-06-23 04:02:42 -0700

    Apply suggestions from code review
    Co-authored-by: Ilya <ilykonst95@gmail.com>

--- src/components/items-list/items-list.component.tsx
+++ src/components/items-list/items-list.component.tsx
@@ -1,7 +1,7 @@
 import type {FC, ReactNode} from 'react';
 import styles from './items-list.module.scss';
 
-const ItemsList: FC<{children: ReactNode}> = ({children}) => (
+const ItemsList: FC = ({children}) => (
   <article className={styles.root}>{children}</article>
 );
 
